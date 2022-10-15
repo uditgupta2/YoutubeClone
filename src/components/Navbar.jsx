7e0acc3f137fa7ee27246f52d00521119b709416
Udit Gupta <udit204gupta@gmail.com>
2022-10-15 17:48:48 +0530

    commit youtube clone

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+
+const Navbar = () => (
+  <div>
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        justifyContent: "space-between",
+        position: "sticky",
+        background: "#000",
+        top: "0",
+      }}
+    >
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
+      <SearchBar />
+    </Stack>
+  </div>
+);
+export default Navbar;
---
+++ src/components/Videos.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  console.log("videos", videos);
+  if (!videos?.length) return "Loading...";
+  return (
+    <Stack
+      direction={direction || "row"}
+      // sx={{ backgroundColor: "black" }}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
